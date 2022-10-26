433510092166001c2828daf3b85270fae6697bf5
Tyomios <tyomios@mail.ru>
2022-10-26 18:31:33 +0700

    Разработка контрола для внесения данных, отображения сообщения и проверки введенного значения на принадлежность к указанному диапазону.

---
+++ src/KompasRingPlugin/Controls/AdvancedTextbox.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace KompasRingPlugin.Controls;
+
+/// <summary>
+/// Interaction logic for AdvancedTextbox.xaml
+/// </summary>
+public partial class AdvancedTextbox : UserControl
+{
+    public AdvancedTextbox()
+    {
+        InitializeComponent();
+    }
+}
