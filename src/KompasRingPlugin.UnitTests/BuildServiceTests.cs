02bfc5b41cac5712d19c5716d0ee2ed7f33697f1
Tyomios <tyomios@mail.ru>
2022-12-01 00:44:32 +0700

    Добавлены тесты для класса гравировки.

--- src/KompasRingPlugin.UnitTests/BuildServiceTests.cs
+++ src/KompasRingPlugin.UnitTests/BuildServiceTests.cs
@@ -1,102 +1,102 @@
-﻿namespace KompasRingPlugin.UnitTests
+﻿namespace KompasRingPlugin.UnitTests;
+
+
+[TestFixture]
+public class BuildServiceTests
 {
-    [TestFixture]
-    public class BuildServiceTests
-    {
-        [Test]
-        public void CreateSketchOnBasePlane_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var service = new BuildService(TODO);
-            BasePlane plane = default(global::Model.BasePlane);
+	[Test]
+	public void CreateSketchOnBasePlane_StateUnderTest_ExpectedBehavior()
+	{
+		// Arrange
+		var service = new BuildService(TODO);
+		BasePlane plane = default(global::Model.BasePlane);
 
-            // Act
-            var result = service.CreateSketchOnBasePlane(
-                plane);
+		// Act
+		var result = service.CreateSketchOnBasePlane(
+			plane);
 
-            // Assert
-            Assert.Fail();
-        }
+		// Assert
+		Assert.Fail();
+	}
 
-        [Test]
-        public void SqueezeOut_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var service = new BuildService(TODO);
-            ksSketchDefinition sketch = null;
-            double height = 0;
-            bool cutMode = false;
-            short blindType = 0;
+	[Test]
+	public void SqueezeOut_StateUnderTest_ExpectedBehavior()
+	{
+		// Arrange
+		var service = new BuildService(TODO);
+		ksSketchDefinition sketch = null;
+		double height = 0;
+		bool cutMode = false;
+		short blindType = 0;
 
-            // Act
-            var result = service.SqueezeOut(
-                sketch,
-                height,
-                cutMode,
-                blindType);
+		// Act
+		var result = service.SqueezeOut(
+			sketch,
+			height,
+			cutMode,
+			blindType);
 
-            // Assert
-            Assert.Fail();
-        }
+		// Assert
+		Assert.Fail();
+	}
 
-        [Test]
-        public void RoundCorners_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var service = new BuildService(TODO);
-            double radius = 0;
-            List roundedEdges = null;
+	[Test]
+	public void RoundCorners_StateUnderTest_ExpectedBehavior()
+	{
+		// Arrange
+		var service = new BuildService(TODO);
+		double radius = 0;
+		List roundedEdges = null;
 
-            // Act
-            service.RoundCorners(
-                radius,
-                roundedEdges);
+		// Act
+		service.RoundCorners(
+			radius,
+			roundedEdges);
 
-            // Assert
-            Assert.Fail();
-        }
+		// Assert
+		Assert.Fail();
+	}
 
-        [Test]
-        public void GetCircleEdges_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var service = new BuildService(TODO);
+	[Test]
+	public void GetCircleEdges_StateUnderTest_ExpectedBehavior()
+	{
+		// Arrange
+		var service = new BuildService(TODO);
 
-            // Act
-            var result = service.GetCircleEdges();
+		// Act
+		var result = service.GetCircleEdges();
 
-            // Assert
-            Assert.Fail();
-        }
+		// Assert
+		Assert.Fail();
+	}
 
-        [Test]
-        public void GetAllFaces_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var service = new BuildService(TODO);
+	[Test]
+	public void GetAllFaces_StateUnderTest_ExpectedBehavior()
+	{
+		// Arrange
+		var service = new BuildService(TODO);
 
-            // Act
-            var result = service.GetAllFaces();
+		// Act
+		var result = service.GetAllFaces();
 
-            // Assert
-            Assert.Fail();
-        }
+		// Assert
+		Assert.Fail();
+	}
 
-        [Test]
-        public void InjectText_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var service = new BuildService(TODO);
-            ksSketchDefinition sketch = null;
-            string text = null;
+	[Test]
+	public void InjectText_StateUnderTest_ExpectedBehavior()
+	{
+		// Arrange
+		var service = new BuildService(TODO);
+		ksSketchDefinition sketch = null;
+		string text = null;
 
-            // Act
-            service.InjectText(
-                sketch,
-                text);
+		// Act
+		service.InjectText(
+			sketch,
+			text);
 
-            // Assert
-            Assert.Fail();
-        }
-    }
+		// Assert
+		Assert.Fail();
+	}
 }
--- src/KompasRingPlugin.UnitTests/EngravingTests.cs
+++ src/KompasRingPlugin.UnitTests/EngravingTests.cs
@@ -4,16 +4,68 @@
     public class EngravingTests
     {
         [Test]
-        public void TestMethod1()
+        public void ConstructorTest()
         {
             // Arrange
+            var expectedText = String.Empty;
+            var expectedTextSize = 4;
+            var expectedHeight = 2;
+
+            // Act
+            var actual = new Engraving();
+
+            // Assert
+            Assert.AreEqual(expectedText, actual.Text);
+            Assert.AreEqual(expectedTextSize, actual.TextSize);
+            Assert.AreEqual(expectedHeight, actual.Height);
+        }
+
+        [Test]
+        public void TextSetterTest()
+        {
+            // Arrange
+            var expectedText = "Тестовые данные";
             var engraving = new Engraving();
 
             // Act
+            engraving.Text = expectedText;
+            var actual = engraving.Text;
 
 
             // Assert
-            Assert.Fail();
+            Assert.AreEqual(expectedText, actual);
+        }
+
+        [Test]
+        public void TextSizeSetterTest()
+        {
+            // Arrange
+            uint expected = 12;
+            var engraving = new Engraving();
+
+            // Act
+            engraving.TextSize = expected;
+            var actual = engraving.TextSize;
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void HeightSetterTest()
+        {
+            // Arrange
+            var expected = 100;
+            var engraving = new Engraving();
+
+            // Act
+            engraving.Height = expected;
+            var actual = engraving.Height;
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
