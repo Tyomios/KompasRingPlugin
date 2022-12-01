2b6e7677fc3c86e79df4c65c20c505b084d835eb
Tyomios <tyomios@mail.ru>
2022-12-01 14:14:41 +0700

    Добавлены тесты для валидатора параметров кольца.

--- src/KompasRingPlugin.UnitTests/RingParamsValidatorTests.cs
+++ src/KompasRingPlugin.UnitTests/RingParamsValidatorTests.cs
@@ -4,18 +4,136 @@
     public class RingParamsValidatorTests
     {
         [Test]
-        public void CheckCorrectValues_StateUnderTest_ExpectedBehavior()
+        public void CheckCorrectValuesTest_ValidateRoundCornersValue()
         {
             // Arrange
-            var ringParamsValidator = new RingParamsValidator();
-            Ring ring = null;
+            var ring = new Ring();
+            ring.Width = 10;
+            ring.RoundScale = 11;
 
             // Act
-            ringParamsValidator.CheckCorrectValues(
-                ring);
+            try
+            {
+                RingParamsValidator.CheckCorrectValues(ring);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Equals("Значение скругления превышает толщину кольца"))
+                {
+                    Assert.Pass();
+                    return;
+                }
+            }
 
             // Assert
             Assert.Fail();
+        }
+
+        [Test]
+        public void CheckCorrectValuesTest_ValidateTextSizeValue_TextSizeBiggerThanRingWidth()
+        {
+            // Arrange
+            var ring = new Ring();
+            ring.Engraving.Text = "Test";
+            ring.Engraving.TextSize = 12;
+            ring.Width = 10;
+
+            // Act
+            try
+            {
+                RingParamsValidator.CheckCorrectValues(ring);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Equals("Значение размера текста превышает толщину кольца"))
+                {
+                    Assert.Pass();
+                    return;
+                }
+            }
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CheckCorrectValuesTest_ValidateTextSizeValue_TextSizeBiggerThanHalfRingWidth()
+        {
+            // Arrange
+            var ring = new Ring();
+            ring.Engraving.Text = "Test";
+            ring.Engraving.TextSize = 7;
+            ring.Width = 12;
+
+            // Act
+            try
+            {
+                RingParamsValidator.CheckCorrectValues(ring);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Equals("Значение размера текста превышает толщину кольца"))
+                {
+                    Assert.Pass();
+                    return;
+                }
+            }
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CheckCorrectValuesTest_ValidateTextSizeValue_ExpectedBehavior()
+        {
+            // Arrange
+            var ring = new Ring();
+            ring.Engraving.Text = "Test";
+            ring.Engraving.TextSize = 5;
+            ring.Width = 12;
+
+            // Act
+            try
+            {
+                RingParamsValidator.CheckCorrectValues(ring);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Equals("Значение размера текста превышает толщину кольца"))
+                {
+                    Assert.Fail();
+                    return;
+                }
+            }
+
+            // Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public void CheckCorrectValuesTest_ValidateRoundScaleValue_ExpectedBehavior()
+        {
+            // Arrange
+            var ring = new Ring();
+            ring.Width = 12;
+            ring.RoundScale = 5;
+
+            // Act
+            try
+            {
+                RingParamsValidator.CheckCorrectValues(ring);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Equals("Значение скругления превышает толщину кольца"))
+                {
+                    Assert.Fail();
+                    return;
+                }
+            }
+
+            // Assert
+            Assert.Pass();
         }
     }
 }
