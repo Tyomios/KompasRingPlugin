68041dd5687c2493ed982995c0da95f52e825bbd
Tyomios <tyomios@mail.ru>
2022-12-06 13:49:01 +0700

    Добавлена модель представления отображаемых данных.

---
+++ src/SecondaryWindow/BaseInfoVM.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SecondaryWindow;
+
+[INotifyPropertyChanged]
+public abstract partial class BaseInfoVM
+{
+    /// <summary>
+    /// Сообщение.
+    /// </summary>
+    [ObservableProperty]
+    private string _message;
+
+    /// <summary>
+    /// Контекст информации.
+    /// </summary>
+    private InfoType _infoType;
+
+    /// <summary>
+    /// Возвращает или задает контекст информации.
+    /// </summary>
+    public InfoType InfoType
+    {
+        get => _infoType;
+        private set
+        {
+            _infoType = value;
+        }
+    }
+}
---
+++ src/SecondaryWindow/InfoType.cs
@@ -0,0 +1,22 @@
+﻿namespace SecondaryWindow;
+
+/// <summary>
+/// Тип отображаемой информации.
+/// </summary>
+public enum InfoType
+{
+    /// <summary>
+    /// Негативное предупреждение.
+    /// </summary>
+    Warning,
+
+    /// <summary>
+    /// Описание прогресса выполнения операции.
+    /// </summary>
+    ProgressStatus,
+
+    /// <summary>
+    /// Успешное завершение операции.
+    /// </summary>
+    SuccessResult
+}
