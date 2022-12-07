d460c1e50c2559d7a53f057d67d5d3fe22c4e2b1
Tyomios <tyomios@mail.ru>
2022-12-07 16:51:03 +0700

    Добавление модели представления дочернего окна ошибки.

---
+++ src/SecondaryWindow/viewModels/WarningVM.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SecondaryWindow.viewModels;
+
+[INotifyPropertyChanged]
+public partial class WarningVM : BaseInfoVM
+{
+    /// <summary>
+    /// Произошедшая ошибка.
+    /// </summary>
+    [ObservableProperty]
+    private string _errorMessage;
+
+    private string _capture;
+
+    public string Capture
+    {
+        get => _capture;
+        private set
+        {
+            _capture = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public WarningVM(string errorMessage, string capture = "Внимание")
+    {
+        ErrorMessage = errorMessage;
+        Capture = capture;
+        Message = "Построение кольца не может быть завершено. Произошла ошибка:";
+    }
+}
