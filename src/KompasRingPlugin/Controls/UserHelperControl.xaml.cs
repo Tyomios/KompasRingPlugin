7d049bc7c8a9956fbeecf6a1c60e1248a7719a1f
Tyomios <tyomios@mail.ru>
2022-12-10 17:44:14 +0700

    Воспроизведение подсказок при выборе блока ввода.

--- src/KompasRingPlugin/Controls/UserHelperControl.xaml.cs
+++ src/KompasRingPlugin/Controls/UserHelperControl.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace KompasRingPlugin.Controls
 {
@@ -25,12 +16,12 @@ namespace KompasRingPlugin.Controls
         /// </summary>
         private static Dictionary<ActionType, string> _actionsAnimations = new ()
         {
-            {ActionType.RoundScale, "file:///../resources/CornerRadiusAnimation.mp4"},
-            {ActionType.EngravingText, "file:///../resources/EngravingTextAnimation.mp4"},
-            {ActionType.EngravingWidth, "file:///../resources/EngravingWidthAnimation.mp4"},
-            {ActionType.RingHeight, "file:///../resources/RingBiggerRadiusAnimation.mp4"},
-            {ActionType.RingSize, "file:///../resources/RingSizeAnimation.mp4"},
-            {ActionType.RingWidth, "file:///../resources/RingWidthAnimation.mp4"}
+            {ActionType.RoundScale, @"resources/CornerRadiusAnimation.mp4"},
+            {ActionType.EngravingText, @"resources/EngravingTextAnimation.mp4"},
+            {ActionType.EngravingWidth, @"resources/EngravingWidthAnimation.mp4"},
+            {ActionType.RingHeight, @"resources/RingBiggerRadiusAnimation.mp4"},
+            {ActionType.RingSize, @"resources/RingSizeAnimation.mp4"},
+            {ActionType.RingWidth, @"resources/RingWidthAnimation.mp4"}
         };
 
         public UserHelperControl()
@@ -47,27 +38,13 @@ namespace KompasRingPlugin.Controls
             {
                 _test = value;
                 addInfoTextBlock.Text = _test.AdditionInfo;
-                MediaElement.Source = new Uri(_actionsAnimations[_test.ToAction]);
+                MediaElement.Source = new Uri(_actionsAnimations[_test.ToAction], UriKind.Relative);
             }
         }
 
-        public HelpParamsUI AdditInfo
+        private void MediaElement_OnMediaFailed(object? sender, ExceptionRoutedEventArgs e)
         {
-            get { return (HelpParamsUI)GetValue(AdditInfoProperty); }
-            set { SetValue(AdditInfoProperty, value); }
-        }
-
-        public static readonly DependencyProperty AdditInfoProperty = DependencyProperty.Register(
-            nameof(AdditInfo), typeof(HelpParamsUI), typeof(AdvancedTextbox),
-            new FrameworkPropertyMetadata(null,
-                FrameworkPropertyMetadataOptions.AffectsRender,
-                new PropertyChangedCallback(OnAddInfoChangedChanged)));
-
-        private static void OnAddInfoChangedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var p = (HelpParamsUI)e.NewValue;
-            ((UserHelperControl)d).addInfoTextBlock.Text = p.AdditionInfo;
-            ((UserHelperControl)d).MediaElement.Source = new Uri(_actionsAnimations[p.ToAction]);
+            MessageBox.Show(e.ErrorException.Message);
         }
     }
 }
